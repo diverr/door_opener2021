db4a8fdcfb564eb5fd1973733a515290144c0530
David Santiago <davidsantiago.fdezbermejo@billin.eu>
2022-10-23 00:17:51 +0200

    add improvements

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from 'react'
-import './App.css'
-import Button from './Button'
-import { Link, Route, useLocation } from 'wouter'
-import Form from './Form'
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Global from './Global'
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import './App.css';
+import Button from './Button';
+import { Link, Route, useLocation } from 'wouter';
+import Form from './Form';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Global from './Global';
+import axios from 'axios';
 
 function App() {
-  const version = process.env.REACT_APP_VERSION
-  console.log('version', version)
+  const version = process.env.REACT_APP_VERSION;
+  console.log('version', version);
 
-  const secret = localStorage.getItem('secret') ?? null
+  const secret = localStorage.getItem('secret') ?? null;
 
-  const [location, setLocation] = useLocation()
+  const [location, setLocation] = useLocation();
 
   useEffect(() => {
     axios
       .get(Global.serviceUrl)
-      .then((response) => {
+      .then(() => {
         toast.success('Servicio preparado', {
           autoClose: 2000,
-        })
-
-        if (secret) {
-          setLocation('/button')
-        }
+        });
       })
       .catch((e) => {
         toast.error(`Error de comunicación: ${e}`, {
           autoClose: 2000,
-        })
-      })
-  }, [])
+        });
+      });
+  }, []);
+
+  if (secret) {
+    setLocation('/button');
+  }
 
   return (
     <div className="App">
@@ -46,7 +46,7 @@ function App() {
       </div>
       <ToastContainer newestOnTop />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,50 +1,97 @@
-import React, { useState } from 'react'
-import remote from './assets/img/remote.png'
-import './Button.css'
-import Global from './Global'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useDoubleTap } from 'use-double-tap'
-import axios from 'axios'
+import React, { useState } from 'react';
+import remote from './assets/img/remote.png';
+import './Button.css';
+import Global from './Global';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useDoubleTap } from 'use-double-tap';
+import axios from 'axios';
+import { useGeolocated } from 'react-geolocated';
 
 export default function Button() {
-  const [opacity, setOpacity] = useState(1)
-  const [opening, setOpening] = useState(false)
+  const [opacity, setOpacity] = useState(1);
+  const [opening, setOpening] = useState(false);
 
-  const secret = localStorage.getItem('secret') ?? null
-  const user = localStorage.getItem('user') ?? null
+  // const url = `${Global.serviceUrl}/${secret}`;
+  // WARNING! change before sending to production
+  const url = `${Global.serviceUrl}`;
+
+  const secret = localStorage.getItem('secret') ?? null;
+  const user = localStorage.getItem('user') ?? null;
+
+  const { coords, isGeolocationEnabled } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 10000,
+  });
+
+  const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+    const deg2rad = (deg) => {
+      return deg * (Math.PI / 180);
+    };
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  };
+
+  const home_lng = process.env.REACT_APP_HOME_LNG;
+  const home_lat = process.env.REACT_APP_HOME_LAT;
 
   const open = async () => {
-    if (opening) return
-    setOpening(true)
+    if (opening) return;
+    setOpening(true);
 
-    // const url = `${Global.serviceUrl}/${secret}`;
-    // WARNING! change before sending to production
-    const url = `${Global.serviceUrl}`
+    console.log({ coords });
+    if (isGeolocationEnabled) {
+      const distance = getDistanceFromLatLonInKm(
+        Number(home_lat),
+        Number(home_lng),
+        coords.latitude,
+        coords.longitude
+      );
 
-    setOpacity(0.2)
+      if (user === 'Guadalupe' && distance > 0.5) {
+        toast.error('Lo siento Guadalupe, no estás en casa', {
+          autoClose: 2000,
+        });
+        setOpening(false);
+        return;
+      }
+    }
+
+    setOpacity(0.2);
 
     toast.info('Abriendo puerta... ', {
       autoClose: 2000,
-    })
+    });
 
     try {
-      await axios.get(url)
+      await axios.get(url);
 
-      const welcome = user === 'Papá' ? 'bienvenido' : 'bienvenida'
+      const welcome = user === 'Papá' ? 'bienvenido' : 'bienvenida';
 
       toast.success(`Puerta abierta, ${welcome} a casa ${user}`, {
         autoClose: 2000,
-      })
+      });
     } catch (e) {
-      toast.error(`Error abriendo la puerta: ${e}`, { autoClose: 2000 })
+      toast.error(`Error abriendo la puerta: ${e}`, { autoClose: 2000 });
     } finally {
-      setOpacity(1)
-      setOpening(false)
+      setOpacity(1);
+      setOpening(false);
     }
-  }
+  };
 
-  const bind = useDoubleTap(open)
+  const bind = useDoubleTap(open);
 
   return (
     <div>
@@ -56,5 +103,5 @@ export default function Button() {
         {...bind}
       />
     </div>
-  )
+  );
 }
